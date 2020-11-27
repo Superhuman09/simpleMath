3dc6a14869ecec53db983b71f94f756fc8bb40ce
timur <email@email>
2020-11-27 16:05:46 +0500

    Final

---
+++ script.js
@@ -0,0 +1,25 @@
+
+var name = prompt('Please enter your name')
+console.log(name)
+
+var age = prompt('Please enter your current age')
+console.log(age)
+
+var nine = prompt ('Please find the solution: 6 + 3'  )
+console.log ('Solution 1: 6 + 3 = 9 Your answer is ' + nine)
+
+var fifteen = prompt ('Please find the solution: 20 - 15')
+console.log('Solution 2: 20 - 5 = 15 Your answer is ' + fifteen)
+
+var thirty = prompt ('Please find the solution: 15 * 2')
+console.log('Solution 3: 15 * 2 = 30 Your answer is ' + thirty)
+
+var two = prompt ('Please find the solution: 8 / 4')
+console.log('Solution 4: 8 / 4 = 2 Your answer is ' + two)
+
+var one = prompt ('Please find the solution: 10 % 3')
+console.log('Solution 4: 10 % 3 = 1 Your answer is ' + one )
+
+alert('Please check your console')
+
+
